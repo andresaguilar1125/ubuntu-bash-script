a2b7f6efaa5dfc7fab16bc28b93fe8138462d6c9
andresaguilar1125 <67489487+andresaguilar1125@users.noreply.github.com>
2024-02-28 15:09:22 -0600

    d

--- virtualbox.sh
+++ virtualbox.sh
@@ -20,9 +20,3 @@ wget https://download.virtualbox.org/virtualbox/7.0.14/Oracle_VM_VirtualBox_Exte
 # *********************************************************************************************
 # Step 3: Install the VirtualBox package through gdebi to install missing dependencies
 sudo gdebi virtualbox-7.0_7.0.14-161095~Ubuntu~jammy_amd64.deb
-
-# There were problems setting up VirtualBox.  To re-start the set-up process, run
-  /sbin/vboxconfig
-as root.  If your system is using EFI Secure Boot you may need to sign the
-kernel modules (vboxdrv, vboxnetflt, vboxnetadp, vboxpci) before you can load
-them. Please see your Linux system's documentation for more information.
