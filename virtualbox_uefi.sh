2846c63360f52835c629e31a1b2c22ea69920856
andresaguilar1125 <67489487+andresaguilar1125@users.noreply.github.com>
2024-02-28 13:38:12 -0600

    e

--- virtualbox_uefi.sh
+++ virtualbox_uefi.sh
@@ -2,7 +2,7 @@
 
 # *********************************************************************************************
 # Step 1: # Generate Key Pair Modules using openssl to sign virtualbox modules
-openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -subj "/CN=virtualbox/"
+openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -subj "/andresvirtualbox/"
 
 # *********************************************************************************************
 # Step 2 : Install mokutil utility for managing Machine Owner Keys (MOKs) on UEFI systems
