7570709b368c65e05fde73e98f8fe21307ff7302
andresaguilar1125 <67489487+andresaguilar1125@users.noreply.github.com>
2024-02-28 13:57:53 -0600

    mok

--- virtualbox_uefi.sh
+++ virtualbox_uefi.sh
@@ -1,43 +1,29 @@
 #!/bin/bash
 
-# *********************************************************************************************
-# Step 1: # Generate Key Pair Modules using openssl to sign virtualbox modules
-openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -subj "/virtualbox=andres/"
-
-# *********************************************************************************************
-# Step 2 : Install mokutil utility for managing Machine Owner Keys (MOKs) on UEFI systems
-# to manage mok keys docs https://manpages.ubuntu.com/manpages/lunar/man1/mokutil.1.html
+# Step 1 : Install mokutil utility for managing Machine Owner Keys (MOKs) on UEFI systems
 sudo apt install mokutil -y
 
-# Enroll Key (`MOK.der`) into the system's MOK list using mokutil. This is necessary for Secure Boot.
+# Step 2 : Generate Key Pair Modules using openssl to sign virtualbox modules
+openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -subj "/CN=ANDRES/"
+
+# Step 3: Enroll Key (`MOK.der`) into the system's MOK list using mokutil. This is necessary for Secure Boot.
 sudo mokutil --import MOK.der
 
-# Sign modules virtualbox (vboxdrv, vboxnetflt, vboxnetadp) using the generated key.
+# Step 4 : Sign modules virtualbox (vboxdrv, vboxnetflt, vboxnetadp) using the generated key.
 sudo apt-get install linux-headers-`uname -r`
 
-# Install DKMS virtualbox
+# Step 5 : Reboot pc especially for Secure Boot to take effect.
+sudo reboot
+
+# REVIEW: Install DKMS virtualbox
 sudo apt install virtualbox-dkms -y
 sudo apt install --reinstall virtualbox-dkms -y
 
-
-
-
-
-
-
-
-
-
-# Sign modules virtualbox (vboxdrv, vboxnetflt, vboxnetadp) using the generated key.
+# REVIEW: Sign modules virtualbox (vboxdrv, vboxnetflt, vboxnetadp) using the generated key.
 sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vboxdrv)
 sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vboxnetflt)
 sudo /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vboxnetadp)
 
-
-
-# *********************************************************************************************
-# Step 3 : Reboot pc especially for Secure Boot to take effect.
-sudo reboot
 
 # *********************************************************************************************
 # Step XX : Remove sign keys (`MOK.priv` and `MOK.der`) from the system
