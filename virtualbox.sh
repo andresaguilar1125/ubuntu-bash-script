b6452f880f876e127edcb8c4cd4692a91aea4c1f
andresaguilar1125 <67489487+andresaguilar1125@users.noreply.github.com>
2024-02-27 22:35:29 -0600

    p

--- virtualbox.sh
+++ virtualbox.sh
@@ -1,8 +1,10 @@
-# Step 1: Create directory to store files in home directory
+# --------------------------------------------
+#  Step 1: Create directory to store files in home directory
 mkdir -p ~/vbox &&
 cd ~/vbox &&
 
-# Step 2: Download files from VirtualBox repository
+# --------------------------------------------
+#  Step 2: Download files from VirtualBox repository
 
 # Download the VirtualBox package for Ubuntu (amd64 architecture)
 wget https://download.virtualbox.org/virtualbox/7.0.14/virtualbox-7.0_7.0.14-161095~Ubuntu~jammy_amd64.deb
@@ -13,32 +15,58 @@ wget https://download.virtualbox.org/virtualbox/7.0.14/VBoxGuestAdditions_7.0.14
 # Download the VirtualBox Extension Pack
 wget https://download.virtualbox.org/virtualbox/7.0.14/Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack
 
-# Step 3: Install the VirtualBox package
+# --------------------------------------------
+#  Step 3: Install the VirtualBox package
 sudo dpkg -i virtualbox-7.0_7.0.14-161095~Ubuntu~jammy_amd64.deb
 
-# Step 4 : Install missing dependencies
+# --------------------------------------------
+#  Step 4 : Install missing dependencies
 sudo apt-get update -y && sudo apt --fix-broken install -y
 
-# Step 5: Reinstall virtualbox-dkms
+# --------------------------------------------
+#  Step 5: Reinstall virtualbox-dkms
 sudo apt install --reinstall virtualbox-dkms -y 
 
-# Step 6: Configure vbox
+# --------------------------------------------
+#  Step 6: Configure vbox
 sudo /sbin/vboxconfig
 
-# Step 7: Add current user to vboxusers group
+# --------------------------------------------
+#  Step 7: Add current user to vboxusers group
 sudo adduser $USER vboxusers
 
-# Step 8: Change host key combination to AltGr
+# --------------------------------------------
+#  Step 8: Change host key combination to AltGr
 VBoxManage setextradata global "GUI/Input/HostKeyCombination" AltGraph
 
-# Step 9 : Accept license and press y
+# --------------------------------------------
+#  Step 9 : Accept license and press y
 sudo VBoxManage extpack install --replace ~/vbox/Oracle_VM_VirtualBox_Extension_Pack-7.0.14.vbox-extpack --accept-license=sha256 
 
-# Step 10: DMKS
+# --------------------------------------------
+#  Step 10: DMKS
 sudo apt install --reinstall linux-headers-$(uname -r) virtualbox-dkms dkms
 
-# Remove virtualbox config and files
-# sudo apt-get remove --purge virtualbox 
-# sudo rm ~/"VirtualBox VMs" -Rf
-# sudo rm ~/.config/VirtualBox/ -Rf
-# sudo rm -f /etc/vbox/vbox.cfg
+# --------------------------------------------
+#  Step 11: Remove vbox config and files
+
+# Remove VirtualBox package
+sudo apt-get remove --purge virtualbox
+
+# Remove VirtualBox VMs directory
+sudo rm ~/"VirtualBox VMs" -Rf
+
+# Remove VirtualBox configuration directory
+sudo rm ~/.config/VirtualBox/ -Rf
+
+# Remove vbox.cfg file
+sudo rm -f /etc/vbox/vbox.cfg
+
+# Remove virtualbox-dkms package
+sudo apt-get remove virtualbox-dkms
+
+# Purge virtualbox-dkms package
+sudo apt-get remove --purge virtualbox-dkms
+
+# Remove any remaining dependencies that were installed automatically and are no longer needed
+sudo apt-get autoremove -y
