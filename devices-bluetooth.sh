afa89f788cd02f47848b18aff4215c27741bbc81
andresaguilar1125 <67489487+andresaguilar1125@users.noreply.github.com>
2024-07-08 19:36:15 -0600

    removed

--- devices-bluetooth.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Add this to startup to remove bluetooth devices
-
-# Remove K380 keyboard
-bluetoothctl remove F4:73:35:89:50:BE
-
-# Remove Microsoft Bluetooth Mouse
-bluetoothctl remove F5:67:60:8F:D3:39
